90704b78d6d50703e5c88a403ecf0fd08f5dcf16
dermiste <thomaspujolle@Thomass-MacBook-Pro.local>
2013-03-08 19:40:50 +0100

    added ip address

--- routes/hub.js
+++ routes/hub.js
@@ -3,5 +3,20 @@
  */
 
 exports.list = function(req, res){
-  res.render('list',{title:"Hub // User list",numConnections:8});
+
+	var ipAddress;
+	var os=require('os');
+	var ifaces=os.networkInterfaces();
+	for (var dev in ifaces) {
+	    var alias=0;
+	    ifaces[dev].forEach(function(details){
+		    if (details.family=='IPv4') {
+		      console.log(dev+(alias?':'+alias:''),details.address);
+		      ip = details.address;
+		      ++alias;
+		    }
+	    });
+    }
+
+  res.render('list',{title:"Hub // User list",numConnections:8,ip:ipAddress});
 };
